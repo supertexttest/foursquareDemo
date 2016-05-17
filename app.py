43f04293a85f23e9ff28915edd52389d0cb4feb1
supertexttest <supertexttest@gmail.com>
2016-05-17 17:58:11 +0530

    use explore apis

--- app.py
+++ app.py
@@ -183,11 +183,11 @@ def makeWebhookResult(data):
         address = location.get('address')
         if address:
             if mobile_menu != "":
-                speech = speech + name + " and address is: " + address + " ,url :" + url + ",mobile menu is:" + mobile_menu + " , "
+                speech = speech + name + " and address is: " + address + " ,url : " + url + ",mobile menu is:" + mobile_menu + " , "
             elif menu_url != "":
-                speech = speech + name + " and address is: " + address + " ,url :" + url + ",menu is:" + menu_url + " , "
+                speech = speech + name + " and address is: " + address + " ,url : " + url + ",menu is:" + menu_url + " , "
             elif url:
-                    speech = speech + name + " and address is: " + address + " ,url :" + url + " , "
+                    speech = speech + name + " and address is: " + address + " ,url : \n" + url + " , "
             else:
                 speech = speech + name + " and address is: " + address + " ,"
             count = count + 1
