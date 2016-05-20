4c162e4bac0fe30f5a814e881e6b970e242349db
supertexttest <supertexttest@gmail.com>
2016-05-20 11:59:44 +0530

    adding foursquareapi

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from apiai import ApiAI
+import datetime
 
 CLIENT_ACCESS_TOKEN = '4f176853c63c4d1897b4c3c1b44b6040'
 ai = ApiAI(CLIENT_ACCESS_TOKEN)
@@ -16,69 +17,151 @@ class Bot:
         result = requests.post(self.base_url, json=payload)
         return result.json()
     def send_text_message_api_ai(self, recipient_id, text):
-        
-        request = ai.text_request()
-        request.lang = 'en'  # optional, default value equal 'en'
-        request.query = text
-        print(request.read())
-        response = request.getresponse()
-       
-        string = response.read().decode('utf-8')
-        print(string)
-        # string_string = str(string)
-        # string_dump = json.dumps(string)
-        # food_order = ""
-        # area_bangalore = ""
-        # low = string.find('food_order')
-        # if low != -1:
-        #   high = string.find('"',low+14)
-        #   if (high - low) > 1:
-        #     food_order = string[low:high]
+        file_out = open("objs_server.txt","r")
+        food_order = file_out.read()
+        file_out.close()
+        with open("objs_server.txt", "w"):
+            pass
+        if food_order != "":
+          send_generic_message(recipient_id,food_order)
+        else:
+          request = ai.text_request()
+          request.lang = 'en'  # optional, default value equal 'en'
+          request.query = text
+          response = request.getresponse()
+    
+          string = response.read().decode('utf-8')
+          print(string)
+          # string_string = str(string)
+          # string_dump = json.dumps(string)
+          food_order = ""
+          area_bangalore = ""
+          low = string.find('food_order')
+          if low != -1:
+            high = string.find('"',low+14)
+            if (high - low) > 1:
+              food_order = string[low:high]
 
-        # low_add = string.find('area_bangalore')
-        # if low_add != -1:
-        #   high_add = string.find('"',low_add+18)
-        #   if (high_add - low_add) > 1:
-        #     area_bangalore = string[low_add:high_add]
+          # low_add = string.find('area_bangalore')
+          # if low_add != -1:
+          #   high_add = string.find('"',low_add+18)
+          #   if (high_add - low_add) > 1:
+          #     area_bangalore = string[low_add:high_add]
 
-        # if food_order:
-        #   file_in = open("objs_server.txt","wb")
-        #   file_in.write(food_order)
-        #   file_in.close()
-        # if area_bangalore:
-        #   file_out = open("objs_server.txt","r")
-        #   food_order = file_out.read()
-        #   file_out.close()
-        #   with open("objs_server.txt", "w"):
-        #       pass
-        
-        index_start = string.find('speech')
-        # print(index_start)
-        index_end = string.find('}',index_start)
-        final_string = string[index_start+9:index_end-1]
-        print(final_string)
+          if food_order != "":
+            file_in = open("objs_server.txt","wb")
+            file_in.write(food_order)
+            file_in.close()
+          # if area_bangalore != "":
+          #   file_out = open("objs_server.txt","r")
+          #   food_order = file_out.read()
+          #   file_out.close()
+          #   with open("objs_server.txt", "w"):
+          #       pass
+          
+          index_start = string.find('speech')
+          # print(index_start)
+          index_end = string.find('}',index_start)
+          final_string = string[index_start+9:index_end-1]
+          print(final_string)
 
-        payload = {'recipient': {'id': recipient_id},
-                   'message': {'text': final_string}
-                  }
-        result = requests.post(self.base_url, json=payload)
-        return result.json()
- 
+          payload = {'recipient': {'id': recipient_id},
+                     'message': {'text': final_string}
+                    }
+          result = requests.post(self.base_url, json=payload)
+          return result.json()
+   
     def send_message(self, recipient_id, message):
         payload ={'recipient': {'id': recipient_id},
                   'message': message
                  }
         return requests.post(self.base_url, json=payload).json()
 
-    def send_generic_message(self, recipient_id, elements):
+    def send_generic_message(self, recipient_id, query):
+
+        baseurl = "https://api.foursquare.com/v2/venues/explore?"
+        date = datetime.datetime.now().strftime ("%Y%m%d")
+        yql_url = baseurl + "query="+query+"&ll=40.7,-74&oauth_token=EMN1IAQ2TNZLHDI0VV3HSCKHWWLV3X3LGMKJV10KTBI55UDL&v=20160516"
+        # if city:
+        #     yql_url = baseurl + "oauth_token=EMN1IAQ2TNZLHDI0VV3HSCKHWWLV3X3LGMKJV10KTBI55UDL&ll=40.7,-74" + city + "&query=" + query + "&v=" + date
+        # else:
+        #     yql_url = baseurl + "oauth_token=EMN1IAQ2TNZLHDI0VV3HSCKHWWLV3X3LGMKJV10KTBI55UDL&ll="+str(lat)+","+str(lng)+"&query=" + query + "&v=" + date
+        # sys.stdout.write(yql_url)
+        result = urllib.urlopen(yql_url).read()
+        data = json.loads(result)
+        # res = makeWebhookResultExplore(data)
+        response = data.get('response')
+          venues = response.get('venues')
+          speech_default = "Sure, I will find places near you. You can go to following places: "
+          speech = ""
+          count = 0
+          for item in venues:
+              if count > 0:
+                  break
+              name = item.get('name')
+              url = item.get('url')
+              menu = item.get('menu')
+              # mobile_menu = ""
+              # menu_url = ""
+              # if menu:
+              #     mobile_menu = menu.get('mobileUrl')
+              #     menu_url = menu.get('url')
+              location = item.get('location')
+              address = location.get('address')
+              count = count + 1
+
+
         payload = {'recipient': {'id': recipient_id},
                    'message': { "attachment": {
                                 "type": "template",
                                 "payload": {
                                     "template_type": "generic",
-                                    "elements": elements
+                                    "thumbnail": url
+                                    "address":address
+                                    "name":name
                                     }
                                 }
                               }
                    }
         return requests.post(self.base_url, json=payload).json()
+
+
+
+      def makeWebhookResult(data):
+          response = data.get('response')
+          venues = response.get('venues')
+          speech_default = "Sure, I will find places near you. You can go to following places: "
+          speech = ""
+          count = 0
+          for item in venues:
+              if count > 2:
+                  break
+              name = item.get('name')
+              url = item.get('url')
+              menu = item.get('menu')
+              mobile_menu = ""
+              menu_url = ""
+              if menu:
+                  mobile_menu = menu.get('mobileUrl')
+                  menu_url = menu.get('url')
+              location = item.get('location')
+              address = location.get('address')
+              if address:
+                  if mobile_menu != "":
+                      speech = speech + name + " and address is: " + address + " ,url : \n" + url + ",mobile menu is:" + mobile_menu + " , "
+                  elif menu_url != "":
+                      speech = speech + name + " and address is: " + address + " ,url : \n" + url + ",menu is:" + menu_url + " , "
+                  elif url:
+                          speech = speech + name + " and address is: " + address + " ,url : \n" + url + " , "
+                  else:
+                      speech = speech + name + " and address is: " + address + " ,"
+                  count = count + 1
+          speech_result = speech_default + speech
+
+          print("Response:")
+          print(speech_result)
+          return {
+              "speech": speech_result,
+              "displayText": speech_result,
+              "source": "apiai-weather-webhook-sample"
+          }
